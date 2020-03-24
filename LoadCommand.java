0f04bc7de340906558ad17509dc1c0440cd21afe
laurenelee99 <59971643+laurenelee99@users.noreply.github.com>
2020-03-23 21:39:47 -0500

    Update LoadCommand.java

--- LoadCommand.java
+++ LoadCommand.java
@@ -1,9 +1,15 @@
-
+/*
+ * @author Camryn Reid, Jason Bell, Lauren Lee
+ */
 public class LoadCommand implements Command {
 	private Document doc;
 	public LoadCommand(Document doc) {
 		this.doc = doc;
 	}
+	/*
+	 * @param none
+	 * @return void
+	 */
 	@Override
 	public void execute() {
 		doc.load();
