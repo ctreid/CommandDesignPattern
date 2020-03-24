47b75360721dc8eb11c6bb585ce93e8a0c56620b
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2020-03-23 20:38:05 -0400

    Update Document.java

--- Document.java
+++ Document.java
@@ -1,18 +1,21 @@
 
 public class Document {
+	static String Name;
 	public Document(String name) {
-		System.out.println(" Documented named " + name + " is created");
+		System.out.println("Documented named " + name + " is created");
+		Name = name;
 	}
-	public void load() {
-		System.out.println("document " + name + " is loaded into view");
+	
+	public static void load() {
+		System.out.println("document " + Name +  " is loaded into view");
 	}
-	public void spell() {
-		System.out.println("document " + name + " is being checked for spelling errors");
+	public static void spell() {
+		System.out.println("document " + Name + " is being checked for spelling errors");
 	}
-	public void save() {
-		System.out.println(" document " + name + " is being saved...");
+	public static void save() {
+		System.out.println("document " + Name + " is being saved...");
 	}
-	public void print() {
-		System.out.println("document " + name + " is printing..");
+	public static void print() {
+		System.out.println("document " + Name + " is printing..");
 	}
 }
