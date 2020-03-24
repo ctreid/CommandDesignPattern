26937aeb58950506b6210a358a0b028a94460b74
laurenelee99 <59971643+laurenelee99@users.noreply.github.com>
2020-03-23 21:43:45 -0500

    Update SaveCommand.java

--- SaveCommand.java
+++ SaveCommand.java
@@ -1,9 +1,15 @@
-
+/*
+ * @author Camryn Reid, Jason Bell, Lauren Lee
+ */
 public class SaveCommand implements Command {
 	private Document doc;
 	public SaveCommand(Document doc) {
 		this.doc = doc;
 	}
+	/*
+	 * @param none
+	 * @return void
+	 */
 	@Override
 	public void execute() {
 		doc.save();
