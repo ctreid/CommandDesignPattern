bc16209c8b42e7751c2c8085a711c1ae5b07058a
Camryn Reid <60122050+ctreid@users.noreply.github.com>
2020-03-23 20:38:33 -0400

    Update InputHandler.java

--- InputHandler.java
+++ InputHandler.java
@@ -1,14 +1,44 @@
 import java.util.HashMap;
 public class InputHandler {
+
+	private HashMap<Command, String> commands = new HashMap<Command, String>();
+
 	public InputHandler(Document document) {
-		HashMap<Command, String> commands = new HashMap<Command, String>(); 
-		commands.put(, "Load");
-	    commands.put(, "Save");
-	    commands.put(, "Spell");
-	    commands.put(, "Print");
-	    System.out.println(commands);
-	}
-	public void inputEntered(String data) {
+		LoadCommand loadCommand = new LoadCommand(document);
+		commands.put(loadCommand, "load");
 		
+		PrintCommand printCommand = new PrintCommand(document);
+		commands.put(printCommand, "print");
+		
+		SaveCommand saveCommand = new SaveCommand(document);
+		commands.put(saveCommand, "save");
+		
+		SpellCommand spellCommand = new SpellCommand(document);
+		commands.put(spellCommand, "spell");
+	}
+	public void inputEntered(String command) {
+		if (command.equals("load")) {
+			Document.load();
+		}
+		
+		else if (command.equals("print")) {
+			Document.print();
+		}
+		
+		else if (command.equals("save")) {
+			Document.save();
+		}
+		
+		else if (command.equals("spell")) {
+			Document.spell();
+		}
+		
+		else if (command.equals("quit")) {
+			System.out.println("Goodbye");
+		}
+		
+		else {
+			System.out.println("Sorry, we don't recognize that command");
+		}
 	}
 }
