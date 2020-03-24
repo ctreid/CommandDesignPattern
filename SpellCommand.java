c974359ea68b505958d786e9265e5dcd6097f455
laurenelee99 <59971643+laurenelee99@users.noreply.github.com>
2020-03-23 21:44:57 -0500

    Update SpellCommand.java

--- SpellCommand.java
+++ SpellCommand.java
@@ -1,8 +1,15 @@
+/*
+ * @author Camryn Reid, Jason Bell, Lauren Lee
+ */
 public class SpellCommand implements Command {
 	private Document doc;
 	public SpellCommand(Document doc) {
 		this.doc = doc;
 	}
+	/*
+	 * @param none
+	 * @return void
+	 */
 	@Override
 	public void execute() {
 		doc.spell();
