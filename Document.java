f7ef6a939b61525ca370f085377f3452ffc432d2
laurenelee99 <59971643+laurenelee99@users.noreply.github.com>
2020-03-23 21:35:29 -0500

    Update Document.java

--- Document.java
+++ Document.java
@@ -1,4 +1,6 @@
-
+/*
+ * @author Jason Bell, Camryn Reid, Lauren Lee
+ * /
 public class Document {
 	static String Name;
 	public Document(String name) {
@@ -6,15 +8,34 @@ public class Document {
 		Name = name;
 	}
 	
+	/ * 
+	  * @param none
+	  * @return void
+	  * /
 	public static void load() {
 		System.out.println("document " + Name +  " is loaded into view");
 	}
+	
+	/ * 
+	  * @param none
+	  * @return void
+	  * /
 	public static void spell() {
 		System.out.println("document " + Name + " is being checked for spelling errors");
 	}
+	
+	/ *
+	  * @param none
+	  * @return void
+	  * /
 	public static void save() {
 		System.out.println("document " + Name + " is being saved...");
 	}
+	
+	/ * 
+	  * @param none
+	  * @return void
+	  * /
 	public static void print() {
 		System.out.println("document " + Name + " is printing..");
 	}
